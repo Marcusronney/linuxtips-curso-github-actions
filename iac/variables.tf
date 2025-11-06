f06848a313593f361a2baf3d271396e3a14b122c
Marcus Rônney <62294289+Marcusronney@users.noreply.github.com>
2025-11-06 20:22:02 -0300

    fix variables

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,14 +1,14 @@
-variable "var.ami_id" {
+variable "ami_id" {
   type = string
-  default = "ami-0dcc1e21636832c5d"
+  default = "ami-0c55b159cbfafe1f0"
 }
 
-variable "var.instance_type" {
+variable "instance_type" {
   type = string
-  dafault = "t3.micro"
+  default = "t3.micro"
 }
 
 variable "variavel_inexistente" {
   type = string
-  dafault = "alor_inexistente"
+  default = "valor_inexistente"
 }
