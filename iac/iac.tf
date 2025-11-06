871469580eb13a09f8558a13282ff77c0dad3ac9
Marcus Rônney <62294289+Marcusronney@users.noreply.github.com>
2025-11-06 19:47:03 -0300

    pipeline terraform

--- iac/iac.tf
+++
@@ -1,3 +0,0 @@
-teste123
-
-teste
---
+++ iac/variables.tf
@@ -0,0 +1,14 @@
+variable "var.ami_id" {
+  type = string
+  default = "ami-0dcc1e21636832c5d"
+}
+
+variable "var.instance_type" {
+  type = string
+  dafault = "t3.micro"
+}
+
+variable "variavel_inexistente" {
+  type = string
+  dafault = "alor_inexistente"
+}
